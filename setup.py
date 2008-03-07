58363ba95715b1e341fe868f601d95c2c1fcc4df
Rob Marianski <rob@marianski.com>
2008-03-07 00:09:58 +0000

    initial structure of opencore.feed
    git-svn-id: https://svn.socialplanning.org/svn/oc-feed/trunk@14144 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='oc-feed',
+      version=version,
+      description="Generate feeds for opencore content",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opencore'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
