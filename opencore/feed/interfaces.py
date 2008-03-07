86394af00194cca95fcd750033b9165188d9ff4d
Rob Marianski <rob@marianski.com>
2008-03-07 16:09:11 +0000

    add titles and descriptions for interfaces and remove unused date field on IFeedData
    git-svn-id: https://svn.socialplanning.org/svn/oc-feed/trunk@14185 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feed/interfaces.py
+++ opencore/feed/interfaces.py
@@ -11,20 +11,33 @@ class ICanFeed(Interface):
 class IFeedData(Interface):
     """interface to expose necessary data for feed creation"""
 
-    title = TextLine()
-    link = ASCII()
-    description = Text()
-    date = TextLine()
-    language = TextLine()
-    pubDate = Datetime()
-    author = TextLine()
-    items = Iterable()
+    title = TextLine(title=u'Feed title',
+                     description=u'Main title of the feed')
+    link = ASCII(title=u'Feed link',
+                 description=u'Link to the object the feed represents')
+    description = Text(title=u'Feed description',
+                       description=u'Description of the feed')
+    language = TextLine(title=u'Feed language',
+                        description=u'Language of the feed')
+    pubDate = Datetime(title=u'Feed date',
+                       description=u'Date that the feed was last updated')
+    author = TextLine(title=u'Feed author',
+                      description=u'Author of the feed')
+    items = Iterable(title=u'Feed items',
+                     description=u'Items the feed is providing')
 
 class IFeedItem(Interface):
     """interface that each feed item should implement"""
 
-    title = TextLine()
-    link = ASCII()
-    description = Text()
-    pubDate = Datetime()
-    body = Text(default=u'', required=False)
+    title = TextLine(title=u'Feed item title',
+                     description=u'Main title of feed item')
+    link = ASCII(title=u'Feed item link',
+                 description=u'Link to the feed item')
+    description = Text(title=u'Feed item description',
+                       description=u'Description of the feed item')
+    pubDate = Datetime(title=u'Feed item date',
+                       description=u'Date updated for the feed item')
+    body = Text(title=u'Feed item body',
+                description=u'Body of the feed item, if any',
+                default=u'',
+                required=False)
