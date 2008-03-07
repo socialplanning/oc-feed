a29bfd0424243d9547d8612d30b5b4fa760fcf92
Jeff Hammel <jhammel@mozilla.com>
2008-03-07 01:57:06 +0000

    conceptually works;  now all i have to do is write code
    git-svn-id: https://svn.socialplanning.org/svn/oc-feed/trunk@14161 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feed/browser/view.py
+++ opencore/feed/browser/view.py
@@ -1,10 +1,10 @@
 from opencore.feed.interfaces import IFeedData
 from Products.Five import BrowserView
 
-class RssView(BrowserView):
+class FeedView(BrowserView):
     """view that simply adapts context to IFeedData
        as promised by marker interface ICanFeed"""
 
     def __init__(self, context, request):
         adapted = IFeedData(context)
-        super(RssView, self).__init__(adapted, request)
+        super(FeedView, self).__init__(adapted, request)
