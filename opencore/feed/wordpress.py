2e394183e9c4ff9a8a90a711341fa550faf49fc5
Jeff Hammel <jhammel@mozilla.com>
2008-04-02 02:13:20 +0000

    rmove pdb
    git-svn-id: https://svn.socialplanning.org/svn/oc-feed/trunk@15378 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feed/wordpress.py
+++ opencore/feed/wordpress.py
@@ -40,7 +40,6 @@ class WordpressFeedAdapter(BaseFeedAdapter):
 
         # pull down the feed with the proper cookie
         req = urllib2.Request(uri)
-        import pdb;  pdb.set_trace()
         cookie = self.context.request.get_header('Cookie')
         if cookie:
             req.add_header('Cookie', cookie)
