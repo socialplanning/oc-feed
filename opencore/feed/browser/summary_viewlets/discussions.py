1a2e252eb3c6c21f077cfbb20a16d6889c29345f
Rob Marianski <rob@marianski.com>
2008-04-14 17:04:57 +0000

    #2558 discussions go before pages
    git-svn-id: https://svn.socialplanning.org/svn/oc-feed/trunk@15720 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feed/browser/summary_viewlets/discussions.py
+++ opencore/feed/browser/summary_viewlets/discussions.py
@@ -2,7 +2,7 @@ from Products.Five.viewlet.viewlet import ViewletBase
 
 class DiscussionsSummaryViewlet(ViewletBase):
 
-    sort_order = 300
+    sort_order = 200
 
     def render(self):
         view = self.context.restrictedTraverse('lists/blank-slate-feed')
--- opencore/feed/browser/summary_viewlets/wiki.py
+++ opencore/feed/browser/summary_viewlets/wiki.py
@@ -2,7 +2,7 @@ from Products.Five.viewlet.viewlet import ViewletBase
 
 class WikiSummaryViewlet(ViewletBase):
 
-    sort_order = 200
+    sort_order = 300
 
     def render(self):
         view = self.context.restrictedTraverse('blank-slate-feed')
