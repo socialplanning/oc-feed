a33331a7c6e100db45ca98cee91b463e3d7ef75d
Jeff Hammel <jhammel@mozilla.com>
2008-04-18 15:11:50 +0000

    feeds now have authorURL;  deprecate and remove the views home method
    git-svn-id: https://svn.socialplanning.org/svn/oc-feed/trunk@15946 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feed/browser/view.py
+++ opencore/feed/browser/view.py
@@ -1,10 +1,6 @@
 from opencore.feed.interfaces import IFeedData
-from opencore.member.utils import portrait_thumb_path
-from opencore.member.utils import profile_path
-from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 from topp.utils.pretty_date import prettyDate
-
 
 class FeedView(BrowserView):
     """view that simply adapts context to IFeedData
@@ -17,14 +13,4 @@ class FeedView(BrowserView):
     def pretty_date(self, date):
         # XXX this is copy/pasted
         return prettyDate(date)
-
-    def home(self, id):
-        """return author home (profile) absolute url"""
-        return '%s/%s' % (self.site_url, profile_path(id))
-
-    @property
-    def site_url(self):
-        portal = getToolByName(self.context.context, 
-                               'portal_url')
-        return portal()
 
