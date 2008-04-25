a7396c80769971916f75aef32d0e98c368f7ffa2
Rob Miller <rob@kalistra.com>
2008-04-25 21:57:02 +0000

    store feed adapters as 'feed' attribute on the viewlet classes instead of overwriting the 'context' attribute
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/oc-feed/branches/plone3@16197 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/feed/browser/view.py
+++ opencore/feed/browser/view.py
@@ -1,14 +1,9 @@
-from opencore.feed.interfaces import IFeedData
 from Products.Five import BrowserView
 from topp.utils.pretty_date import prettyDate
 
 class FeedView(BrowserView):
     """view that simply adapts context to IFeedData
        as promised by marker interface ICanFeed"""
-
-    def __init__(self, context, request):
-        adapted = IFeedData(context)
-        super(FeedView, self).__init__(adapted, request)
 
     def pretty_date(self, date):
         # XXX this is copy/pasted
